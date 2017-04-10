9f17aecf46359544388bacea8efffaa5092de2b9
Son Thai <thaison242@gmail.com>
2017-04-09 23:01:41 -0700

    Fix sequence_diagram.sh script and pom.xml file

--- scripts/sequence_diagram.sh
+++ scripts/sequence_diagram.sh
@@ -1,4 +1,4 @@
 echo "Clean up sequence diagram source code"
-rm ../SequenceDiagram/src/aspectj/code/*.class ../SequenceDiagram/src/aspectj/sequencediagram/*.class
+rm ../SequenceDiagram/src/aspectj/sequencediagram/*.class
 echo "Compile sequence diagram source code"
-ajc -1.8 ../SequenceDiagram/src/aspectj/code/*.java ../SequenceDiagram/src/aspectj/sequencediagram/*.aj 
+ajc -1.8 ../SequenceDiagram/src/aspectj/sequencediagram/*.java ../SequenceDiagram/src/aspectj/sequencediagram/*.aj 
